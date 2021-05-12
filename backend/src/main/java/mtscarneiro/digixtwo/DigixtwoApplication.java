051c808ded04804ac040621b70ab03e88594287f
mtscarneiro <mchcar@vivaldi.net>
2021-05-12 16:17:20 -0400

    project created

---
+++ backend/src/main/java/mtscarneiro/digixtwo/DigixtwoApplication.java
@@ -0,0 +1,13 @@
+package mtscarneiro.digixtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigixtwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigixtwoApplication.class, args);
+	}
+
+}
